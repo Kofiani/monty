39ac78352341266be78d3ea4b56655362a2ac4d8
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-08 22:30:23 +0100

    declared a function pint that prints the value at the top of the stack

--- stack.c
+++ stack.c
@@ -58,3 +58,22 @@ void _pall(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
 		runner = runner->next;
 	}
 }
+
+/**
+ * _pint - print int a top of stack
+ * @stack: pointer to linked list stack
+ * @line_number: number of line opcode occurs on
+ */
+
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *runner;
+
+	runner = *stack;
+	if (runner == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		error_exit(stack);
+	}
+	printf("%d\n", runner->n);
+}
