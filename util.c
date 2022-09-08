c223f13f5818ed5e77aa3be1a8f4622c574ac17b
Ivie Osoiye <ivieosoiye@gmail.com>
2022-09-08 23:33:00 +0100

    implemented the rotl opcode that rotates the stack to the top

--- util.c
+++ util.c
@@ -41,3 +41,39 @@ int isnumber(char *str)
 	}
 	return (1);
 }
+
+
+/**
+ * _rotl - rotates the list left
+ * @stack: pointer to the top of the stack
+ * @line_number: the index of the current line
+ *
+ */
+
+
+void _rotl(stack_t **stack, __attribute__ ((unused))unsigned int line_number)
+{
+	stack_t *runner;
+	int temp1, temp2;
+
+	if (*stack == NULL)
+		return;
+	runner = *stack;
+	while (runner->next)
+		runner = runner->next;
+	while (runner)
+	{
+		if (!runner->next)
+		{
+			temp1 = runner->n;
+			runner->n = (*stack)->n;
+		}
+		else
+		{
+			temp2 = runner->n;
+			runner->n = temp1;
+			temp1 = temp2;
+		}
+		runner = runner->prev;
+	}
+}
